cc02e5feb575a0964255ebdfaa3aa528035eb8cf
GabrAlmeid <85247121+GabrAlmeid@users.noreply.github.com>
2021-12-14 13:34:11 -0300

    for e while

---
+++ script2.js
@@ -0,0 +1,18 @@
+var numero = 5;
+
+for(var i = 0; i < numero; i++){
+
+    console.log("Executando o for, pela " + i + " vez")
+}
+console.log("Acabou");
+
+
+var number = Math.random() * 100;
+
+while (number < 90){
+    console.log (number);
+    number = Math.random() * 100;
+}
+
+console.log (number);
+console.log ("Acabou");
