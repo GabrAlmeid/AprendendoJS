f017b088b17a589a674ff318e991f06b8200a359
GabrAlmeid <85247121+GabrAlmeid@users.noreply.github.com>
2021-12-14 14:04:05 -0300

    Array

---
+++ script3.js
@@ -0,0 +1,23 @@
+/*
+var alunos = new Array("Gabriel", "João", "Marcos", "Maria")
+console.log (alunos [1]);
+*/
+
+/*var alunos = ["Gabriel", "João", "Marcos", "Maria", "Mariana"]
+console.log (alunos);
+*/
+
+/*
+var alunos = ["Gabriel", "João", "Marcos", "Maria", "Mariana", "Ana", "Arthur"]
+
+for (var i = 0; i < alunos.length; i++){
+console.log (alunos[i])
+}
+*/
+
+var alunos = ["Gabriel", "João", "Marcos", "Maria", "Mariana", "Ana", "Arthur"]
+
+for (var aluno of alunos ){
+console.log (aluno)
+}
+
