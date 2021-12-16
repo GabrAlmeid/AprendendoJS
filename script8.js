c163c52087e9ca03301300125a54c34f3f4ec3a5
GabrAlmeid <85247121+GabrAlmeid@users.noreply.github.com>
2021-12-16 14:10:17 -0300

    Acessando e modificando o HTML com JS

---
+++ script8.js
@@ -0,0 +1,45 @@
+//console.log (document)
+//console.log (document.body)
+//console.log (document.body.innerHTML)
+
+//document.body.innerHTML = "<h1> Esse titulo foi inserido pelo js<h1>" + document.body.innerHTML
+
+//let p1 = document.getElementById("p1") 
+//console.log(p1)
+
+/*
+let paragrafos = document.getElementsByClassName("paragrafo")
+console.log(paragrafos)
+
+for (let p of paragrafos){
+    console.log(p)
+
+}
+
+let tp = document.getElementsByTagName("p")
+console.log(tp)
+*/
+
+//Modificando HTML
+
+/*
+let p = document.getElementById("p1")
+
+p.innerHTML = "Novo conteúdo"
+p.innerText = "Conteúdo 2"
+p.textContent = "Conteúdo 3"
+p.innerHTML = "<h1> Novo conteúdo com h1 </h1>"
+p.className = ""
+p.style = "color: red"
+
+let meuAtt = p.getAttribute("meuAtt")
+meuAtt
+
+let meuAtt = p.setAttribute("meuAtt", "outro nome")
+*/
+
+
+
+
+
+
