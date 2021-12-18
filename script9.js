dfe08bd355a36c0d83e11e78e123562083f439af
GabrAlmeid <85247121+GabrAlmeid@users.noreply.github.com>
2021-12-18 14:37:55 -0300

    Biblioteca Math

---
+++ script9.js
@@ -0,0 +1,28 @@
+//Alguns elementos math para entender
+
+//let pi = Math.PI;
+//console.log(pi);
+
+//let n = Math.E;
+//console.log(n);
+
+//let n = Math.sqrt(40);
+//console.log(n);
+
+//let n = Math.pow(2, 3);
+//console.log(n);
+
+//let n = Math.random();
+//console.log(n);
+
+//let n = Math.round (Math.random() * 10);
+//console.log(n);
+
+//let n = Math.round(5.5);
+//console.log(n);
+
+//let n = Math.floor(5.99);
+//console.log(n);
+
+let n = Math.ceil(5.1);
+console.log(n);
